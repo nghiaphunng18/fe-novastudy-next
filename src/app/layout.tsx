0ca5b981bfc6635191256f345acdad9e9c589c8b
nghiaphunng <duynghia135z@gmail.com>
2025-04-07 13:32:23 +0700

    design component menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
+'use client';
 import * as React from 'react';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from '@/theme';
+import { ThemeProvider } from "@mui/material";
 import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
-import ModeSwitch from '@/components/ModeSwitch';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '@/components/theme-registry/theme';
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
@@ -13,9 +13,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
         <InitColorSchemeScript attribute="class" />
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
             <CssBaseline />
-            <ModeSwitch />
             {props.children}
           </ThemeProvider>
         </AppRouterCacheProvider>
